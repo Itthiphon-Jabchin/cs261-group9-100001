aa5dab5bb9b549edfb0b382a3f64c1ccf8810824
Itthiphon <itthiphon.jab@dome.tu.ac.th>
2025-10-14 21:30:09 +0700

    IDK

--- mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/CampusJobsApplication.java
+++ mini project/uni-jobs-portal/src/main/java/com/example/campusjobs/CampusJobsApplication.java
@@ -1,11 +1,32 @@
 package com.example.campusjobs;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CampusJobsApplication {
+    // Inject ค่าจาก application.properties เข้ามาในตัวแปร
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
     public static void main(String[] args) {
         SpringApplication.run(CampusJobsApplication.class, args);
+    }
+
+    // สร้าง Bean ที่จะทำงานตอนสตาร์ทแอป
+    @Bean
+    public CommandLineRunner printDbConfigRunner() {
+        return args -> {
+            System.out.println("==================================================");
+            System.out.println("          DATABASE DEBUG INFO         ");
+            System.out.println("Username read from properties: [" + dbUsername + "]");
+            System.out.println("Password read from properties: [" + dbPassword + "]");
+            System.out.println("==================================================");
+        };
     }
 }
